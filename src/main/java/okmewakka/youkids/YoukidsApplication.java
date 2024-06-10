b30ffcc4246b8f7e1bf56a27db21a0a4b48be8c7
jiyuuuuun <hwangjy001@naver.com>
2024-06-11 01:28:13 +0900

    CORS

--- src/main/java/okmewakka/youkids/YoukidsApplication.java
+++ src/main/java/okmewakka/youkids/YoukidsApplication.java
@@ -2,8 +2,10 @@ package okmewakka.youkids;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"okmewakka.youkids", "okmewakka.youkids.config"})
 public class YoukidsApplication {
 
 	public static void main(String[] args) {
