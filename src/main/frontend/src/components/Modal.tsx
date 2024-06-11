a6dcaae1bfa706fa121bca8983b290ec92e7f192
walter230 <ansemr99@gmail.com>
2024-06-11 13:44:23 +0900

    .

--- src/main/frontend/src/components/Modal.tsx
+++ src/main/frontend/src/components/Modal.tsx
@@ -1,27 +1,42 @@
-import { ReactNode } from "react";
+import React from "react";
 
-interface ModalType {
-  children?: ReactNode;
+interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
+  imagePaths: string[];
 }
 
-export default function Modal(props: ModalType) {
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, imagePaths }) => {
+  if (!isOpen) return null;
+
   return (
-    <>
-      {props.isOpen && (
-        <div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70"
-          onClick={props.onClose}
-        >
-          <div
-            className="bg-[#DAEBE5] w-[312px] h-[362px] p-4 rounded-lg"
-            onClick={(e) => e.stopPropagation()}
-          >
-            {props.children}
-          </div>
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center">
+      <div className="bg-white p-4 rounded shadow-lg">
+        <div className="flex items-center justify-center pb-2">
+          <button className="text-sm pr-6" onClick={onClose}>
+            취소
+          </button>
+          <input
+            className="bg-white w-[180px] h-[30px] rounded-lg text-center flex items-center justify-center"
+            placeholder="제목"
+          ></input>
+          <button className="text-sm pl-6" onClick={onClose}>
+            저장
+          </button>
         </div>
-      )}
-    </>
+        <div className="flex flex-wrap justify-center">
+          {imagePaths.map((path, index) => (
+            <img
+              key={index}
+              src={path}
+              alt={`Photo ${index}`}
+              className="w-[90px] h-[90px] m-1"
+            />
+          ))}
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default Modal;
