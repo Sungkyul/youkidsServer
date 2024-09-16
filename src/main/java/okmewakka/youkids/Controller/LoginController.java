1dc31d62de2eb2602f1105fb6777d5ee48e611ed
walter230 <ansemr99@gmail.com>
2024-09-16 17:03:06 +0900

    로그인 완성

--- src/main/java/okmewakka/youkids/Controller/LoginController.java
+++ src/main/java/okmewakka/youkids/Controller/LoginController.java
@@ -2,6 +2,7 @@ package okmewakka.youkids.Controller;
 
 
 import jakarta.servlet.http.HttpSession;
+import okmewakka.youkids.DTO.LoginRequest;
 import okmewakka.youkids.Repository.UserRepository;
 import okmewakka.youkids.Service.UserService;
 import okmewakka.youkids.entity.user;
@@ -14,6 +15,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
 
 @Tag(name = "로그인 API", description = "로그인 API")
 @Controller
@@ -26,19 +29,19 @@ public class LoginController {
     private UserRepository userRepository;
 
 
-    @Operation(summary = "로그인", description = "입력 받은 전화번호, 비밀번호로 로그인.")
-    @Parameter(name = "phoneNumber", description = "입력한 전화번호")
-    @Parameter(name = "password", description = "입력한 비밀번호")
+     @Operation(summary = "로그인", description = "입력 받은 전화번호, 비밀번호로 로그인.")
     @PostMapping("/login")
-    public String login(@RequestParam String phoneNumber, @RequestParam String password, HttpSession session, Model model) {
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest, HttpSession session) {
+        String phoneNumber = loginRequest.getPhoneNumber();
+        String password = loginRequest.getPassword();
+
         if (userService.authenticate(phoneNumber, password)) {
             user user = userRepository.findByUserIdPhone(phoneNumber);
-            String username=user.getUserName();
+            String username = user.getUserName();
             session.setAttribute("username", username);
-            return "redirect:/dashboard";
+            return ResponseEntity.ok().build(); // 성공 응답
         } else {
-            model.addAttribute("error", "Invalid username or password");
-            return "index";
+            return ResponseEntity.status(401).body("Invalid username or password"); // 인증 실패 응답
         }
     }
 
