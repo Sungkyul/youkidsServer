730562320836274bac6928063658a2750a71412c
walter230 <ansemr99@gmail.com>
2024-10-28 22:06:51 +0900

    메뉴창 외부 클릭시 닫힘

--- src/main/frontend/src/components/Menu.tsx
+++ src/main/frontend/src/components/Menu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import MenuIcon from "../assets/menu.svg";
 import CheckIcon from "../assets/check.svg";
 
@@ -15,6 +15,27 @@ const MenuButton: React.FC<MenuButtonProps> = ({
   onShowFavorites,
   showFavoritesOnly,
 }) => {
+  const menuRef = useRef<HTMLDivElement | null>(null); // 메뉴 리스트를 참조하기 위한 ref
+
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      onClick(); // 메뉴를 닫는 함수 호출
+    }
+  };
+
+  useEffect(() => {
+    if (visible) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    } else {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    }
+
+    // 컴포넌트 언마운트 시 이벤트 리스너 정리
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [visible]);
+
   const handleButtonClick = () => {
     onClick();
   };
@@ -25,7 +46,10 @@ const MenuButton: React.FC<MenuButtonProps> = ({
         <img src={MenuIcon} alt="메뉴" />
       </button>
       {visible && (
-        <div className="mr-4 mt-1 absolute top-12 right-0 bg-white shadow-md rounded-md z-50">
+        <div
+          ref={menuRef}
+          className="mr-4 mt-1 absolute top-12 right-0 bg-white shadow-md rounded-md z-50"
+        >
           <ul className="flex flex-col py-2">
             <li
               className="py-1 pl-3 hover:bg-gray-200 cursor-pointer flex items-center"
@@ -37,7 +61,7 @@ const MenuButton: React.FC<MenuButtonProps> = ({
                 <img src={CheckIcon} alt="체크 표시" className="ml-2 w-4 h-4" />
               )}{" "}
             </li>
-            <li className="py-1 px-3 hover:bg-gray-200 cursor-pointer">수정</li>
+            <li className="py-1 px-3 hover:bg-gray-200 cursor-pointer">선택</li>
             <li className="py-1 px-3 hover:bg-gray-200 cursor-pointer">삭제</li>
           </ul>
         </div>
